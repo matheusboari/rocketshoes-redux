a16133223aca6694a39d3634d2a1bfe7d6032d38
Matheus Boari <matheusboari@gmail.com>
2019-11-02 07:05:11 -0300

    Create home :sparkles:

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,102 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis top</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis top</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis top</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis top</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis top</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-person-masculino/05/D22-3253-405/D22-3253-405_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis top</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
