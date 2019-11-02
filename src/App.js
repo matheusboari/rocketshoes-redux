4481c16d1ddcb009b03e55e0710ae10f707e473c
Matheus Boari <matheusboari@gmail.com>
2019-11-02 06:42:49 -0300

    Add global styles :lipstick:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 export default class App extends Component {
@@ -9,6 +10,7 @@ export default class App extends Component {
       <BrowserRouter>
         {/* <Header /> */}
         <Routes />
+        <GlobalStyle />
       </BrowserRouter>
     );
   }
